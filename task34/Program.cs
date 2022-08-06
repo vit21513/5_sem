d790fdaea35654a5d2163a0601dde7ccd19634ef
«vitaly» <vit21513@yandex.ru>
2022-08-06 07:27:42 +0300

    add task 36

--- task34/Program.cs
+++ task34/Program.cs
@@ -2,7 +2,7 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-int[] CreateArrayRndInd (int size, int min, int max)
+int[] CreateArrayRndInd(int size, int min, int max)
 {
     int[] arr = new int[size];
     Random rnd = new Random();
@@ -15,8 +15,8 @@ int[] CreateArrayRndInd (int size, int min, int max)
 }
 
 
-int EventNumArray (int[] array)
-{   
+int EventNumArray(int[] array)
+{
     int evenNum = 0;
     for (int i = 0; i < array.Length; i++)
     {
@@ -33,17 +33,17 @@ void PrintArray(int[] array)
     for (int i = 0; i < array.Length; i++)
     {
 
-        if(i == 0) Console.Write("[");
-        if(i < array.Length - 1) Console.Write(array[i] + ", ");
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
         else Console.Write(array[i] + "]");
     }
-    }
+}
 
 
 int[] array = CreateArrayRndInd(12, 99, 999);
 PrintArray(array);
 Console.WriteLine();
-int result = EventNumArray (array);
+int result = EventNumArray(array);
 Console.WriteLine($" количество чётных чисел в массиве составляет {result} ");
 
 
---
+++ task36/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами.
+ //Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInd(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int SummOddArray(int[] array)
+{
+    int summOdd = 0;
+    for (int i = 1; i < array.Length; i=i+2)
+    {
+         summOdd=  summOdd+ array[i];         
+    }
+    return summOdd;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+int[] array = CreateArrayRndInd(4, -11, 10);
+PrintArray(array);
+Console.WriteLine();
+int result = SummOddArray(array);
+Console.WriteLine($" сумма элементов стоящих на нечётных позициях в массиве составляет {result} ");
